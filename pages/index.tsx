67567f1e2a60d5ffa48ebb79248c452fda2c01ab
Suizzle <andygolay@gmail.com>
2024-01-14 17:16:59 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Face Photo Restorer</title>
+        <title>🐓 Coqify Your Profile</title>
       </Head>
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-20">
@@ -22,21 +22,21 @@ const Home: NextPage = () => {
           rel="noreferrer"
           className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out"
         >
-          Used by over <span className="font-semibold">470,000</span> happy
+          Used by over <span className="font-semibold">42069</span> happy
           users
         </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">
-          Restoring old photos{' '}
+          Add a $COQ ring
           <span className="relative whitespace-nowrap text-[#3290EE]">
             <SquigglyLines />
-            <span className="relative">using AI</span>
+            <span className="relative">to your profile</span>
           </span>{' '}
-          for everyone.
+          in under a second.
         </h1>
 
         <p className="mx-auto mt-12 max-w-xl text-lg text-slate-700 leading-7">
-          Have old and blurry face photos? Let our AI restore them so those
-          memories can live on. 100% free – restore your photos today.
+          What if you could very easily and subtly Coqify your pfp using the famous orange/yellow circle in the Coq Inu logo, placed around your current PFP?? 
+          Subtle, but easily visible.
         </p>
         <div className="flex justify-center space-x-4">
           <a
@@ -46,7 +46,7 @@ const Home: NextPage = () => {
             rel="noreferrer"
           >
             <button onClick={() => va.track('RoomGPT link clicked')}>
-              Check out roomGPT
+              $COQ Lite
             </button>
           </a>
 
@@ -54,16 +54,22 @@ const Home: NextPage = () => {
             className="bg-black rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-black/80"
             href="/restore"
           >
-            Restore your photos
+            $COQ Ring
+          </Link>
+          <Link
+            className="bg-black rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-black/80"
+            href="/restore"
+          >
+            Full Penetration
           </Link>
         </div>
         <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
             <div className="flex sm:space-x-2 sm:flex-row flex-col">
               <div>
-                <h2 className="mb-1 font-medium text-lg">Original Photo</h2>
+                <h2 className="mb-1 font-medium text-lg">Original PFP</h2>
                 <Image
-                  alt="Original photo of my bro"
+                  alt="Original Twitter / X PFP"
                   src="/michael.jpg"
                   className="w-96 h-96 rounded-2xl"
                   width={400}
@@ -71,9 +77,9 @@ const Home: NextPage = () => {
                 />
               </div>
               <div className="sm:mt-0 mt-8">
-                <h2 className="mb-1 font-medium text-lg">Restored Photo</h2>
+                <h2 className="mb-1 font-medium text-lg">Coqified Photo</h2>
                 <Image
-                  alt="Restored photo of my bro"
+                  alt="Coqified PFP"
                   width={400}
                   height={400}
                   src="/michael-new.jpg"
@@ -84,7 +90,6 @@ const Home: NextPage = () => {
           </div>
         </div>
       </main>
-      <Testimonials />
       <Footer />
     </div>
   );
