87390990b99594c2a972a57bb0a06ffe58f814a8
Suizzle <andygolay@gmail.com>
2024-01-14 23:10:29 +0000

    text

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ const Home: NextPage = () => {
         </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">
           Add a $COQ ring
-          <span className="relative whitespace-nowrap text-[#3290EE]">
+          <span className="relative whitespace-nowrap text-[#fcce5f]">
             <SquigglyLines />
             <span className="relative">to your profile</span>
           </span>{' '}
