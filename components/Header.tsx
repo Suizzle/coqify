dcf00b41c7da6d5535cbc62bc7d410e3324aab87
Suizzle <andygolay@gmail.com>
2024-01-15 00:54:34 +0000

    border

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Header({ photo }: { photo?: string | undefined }) {
   return (
-    <header className="flex justify-between items-center w-full mt-5 border-t border-yellow-300 pb-7 sm:px-4 px-2">
+    <header className="flex justify-between items-center w-full mt-5 border-b border-yellow-300 pb-7 sm:px-4 px-2">
       <Link href="/" className="flex space-x-2">
         <Image
           alt="header text"
