49588d2f42c5d06e6255ccb3f5eeea2873843c4f
Suizzle <andygolay@gmail.com>
2024-01-15 19:33:48 +0000

    fixed head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,13 @@
-import Document, { Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          
+        </Head>
         <body>
           <Main />
           <NextScript />
